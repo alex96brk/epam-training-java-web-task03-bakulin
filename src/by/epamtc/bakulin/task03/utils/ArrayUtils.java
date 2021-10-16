127374035ce9cfdb08c4462c352bff12c61d38eb
Alexey <alexey.bakulin96@gmail.com>
2021-10-16 18:09:38 +0300

    [add]: sortArrayBubble() impl added

--- src/by/epamtc/bakulin/task03/utils/ArrayUtils.java
+++ src/by/epamtc/bakulin/task03/utils/ArrayUtils.java
@@ -1,4 +1,20 @@
 package by.epamtc.bakulin.task03.utils;
 
+import by.epamtc.bakulin.task03.entity.PlainArray;
+
 public class ArrayUtils {
+
+    public static void sortArrayBubble(PlainArray arrayToSort) {
+        int[] array = arrayToSort.getArrayData();
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int buffer = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = buffer;
+                }
+            }
+        }
+        arrayToSort.setArrayData(array);
+    }
 }
