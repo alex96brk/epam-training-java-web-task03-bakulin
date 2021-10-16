e67b7c74459a595d7f3b82991eac4f3c4bccabdc
Alexey <alexey.bakulin96@gmail.com>
2021-10-17 00:41:41 +0300

    [add]: test ArrayUtils sorters implementation added

--- src/by/epamtc/bakulin/task03/test/ArrayUtilsTest.java
+++ src/by/epamtc/bakulin/task03/test/ArrayUtilsTest.java
@@ -14,13 +14,86 @@ public class ArrayUtilsTest {
 
     @Test
     public void testArrayUtilsA1() {
+        System.out.println("Test1: Bubble sort Ascending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        ArrayUtils.sortArrayBubble(array1);
-        System.out.println(String.format("Sorted Array: %s", array1.toString()));
+        ArrayUtils.sortArrayBubbleAsc(array1);
+        System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
+        System.out.println("Test1: Bubble sort - Finished\n");
     }
+
+    @Test
+    public void testArrayUtilsA2() {
+        System.out.println("Test1: Bubble sort Descending - Started");
+        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
+        PlainArray array1 = new Array(arr);
+        System.out.println(String.format("Current Array: %s", array1.toString()));
+
+        ArrayUtils.sortArrayBubbleDesc(array1);
+        System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
+
+        System.out.println("Test1: Bubble sort - Finished\n");
+    }
+
+    @Test
+    public void testArrayUtilsB1() {
+        System.out.println("Test2: Selection sort Ascending - Started");
+        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
+        PlainArray array1 = new Array(arr);
+        System.out.println(String.format("Current Array: %s", array1.toString()));
+
+        ArrayUtils.sortArraySelectionAsc(array1);
+        System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
+
+        System.out.println("Test2: Selection sort - Finished\n");
+    }
+
+    @Test
+    public void testArrayUtilsB2() {
+        System.out.println("Test2: Selection sort Descending - Started");
+        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
+        PlainArray array1 = new Array(arr);
+        System.out.println(String.format("Current Array: %s", array1.toString()));
+
+        ArrayUtils.sortArraySelectionDesc(array1);
+        System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
+
+        System.out.println("Test2: Selection sort - Finished\n");
+    }
+
+    @Test
+    public void testArrayUtilsC1() {
+        System.out.println("Test3: Quick sort Ascending - Started");
+        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
+        PlainArray array1 = new Array(arr);
+        System.out.println(String.format("Current Array: %s", array1.toString()));
+
+        ArrayUtils.sortArrayQuickAsc(array1);
+        System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
+
+        System.out.println("Test3: Quick sort - Finished\n");
+    }
+
+    @Test
+    public void testArrayUtilsC2() {
+        System.out.println("Test3: Quick sort Descending - Started");
+        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
+        PlainArray array1 = new Array(arr);
+        System.out.println(String.format("Current Array: %s", array1.toString()));
+
+        ArrayUtils.sortArrayQuickDesc(array1);
+        System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
+
+        System.out.println("Test3: Quick sort - Finished\n");
+    }
+
 }
