18ff747f360abab32f25eabab4bd2b6f02a12e52
Alexey <alexey.bakulin96@gmail.com>
2021-10-17 19:35:40 +0300

    [add]: test for findAllFibonacci(array) impl added

--- src/by/epamtc/bakulin/task03/test/ArrayUtilsTest.java
+++ src/by/epamtc/bakulin/task03/test/ArrayUtilsTest.java
@@ -28,7 +28,7 @@ public class ArrayUtilsTest {
 
     @Test
     public void testArrayUtilsA2() {
-        System.out.println("Test1: Bubble sort Descending - Started");
+        System.out.println("Test2: Bubble sort Descending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -37,12 +37,12 @@ public class ArrayUtilsTest {
         ArrayUtils.sortArrayBubbleDesc(array1);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
-        System.out.println("Test1: Bubble sort - Finished\n");
+        System.out.println("Test2: Bubble sort - Finished\n");
     }
 
     @Test
     public void testArrayUtilsB1() {
-        System.out.println("Test2: Selection sort Ascending - Started");
+        System.out.println("Test3: Selection sort Ascending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -51,12 +51,12 @@ public class ArrayUtilsTest {
         ArrayUtils.sortArraySelectionAsc(array1);
         System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
-        System.out.println("Test2: Selection sort - Finished\n");
+        System.out.println("Test3: Selection sort - Finished\n");
     }
 
     @Test
     public void testArrayUtilsB2() {
-        System.out.println("Test2: Selection sort Descending - Started");
+        System.out.println("Test4: Selection sort Descending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -65,12 +65,12 @@ public class ArrayUtilsTest {
         ArrayUtils.sortArraySelectionDesc(array1);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
-        System.out.println("Test2: Selection sort - Finished\n");
+        System.out.println("Test4: Selection sort - Finished\n");
     }
 
     @Test
     public void testArrayUtilsC1() {
-        System.out.println("Test3: Quick sort Ascending - Started");
+        System.out.println("Test5: Quick sort Ascending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -79,12 +79,12 @@ public class ArrayUtilsTest {
         ArrayUtils.sortArrayQuickAsc(array1);
         System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
-        System.out.println("Test3: Quick sort - Finished\n");
+        System.out.println("Test5: Quick sort - Finished\n");
     }
 
     @Test
     public void testArrayUtilsC2() {
-        System.out.println("Test3: Quick sort Descending - Started");
+        System.out.println("Test6: Quick sort Descending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -93,12 +93,12 @@ public class ArrayUtilsTest {
         ArrayUtils.sortArrayQuickDesc(array1);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
-        System.out.println("Test3: Quick sort - Finished\n");
+        System.out.println("Test6: Quick sort - Finished\n");
     }
 
     @Test
     public void testArrayUtilsD1() {
-        System.out.println("Test3: Binary Search - Started");
+        System.out.println("Test7: Binary Search - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -108,12 +108,12 @@ public class ArrayUtilsTest {
         int resIndex = ArrayUtils.binarySearch(4, array1);
         System.out.println(String.format("result index = %d", resIndex));
         System.out.println(String.format("Sorted Array: %s", array1.toString()));
-        System.out.println("Test3: Binary Search - Finished\n");
+        System.out.println("Test7: Binary Search - Finished\n");
     }
 
     @Test
     public void testArrayUtilsE1() {
-        System.out.println("Test3: Search Minimal value - Started");
+        System.out.println("Test8: Search Minimal value - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -122,12 +122,12 @@ public class ArrayUtilsTest {
         int minimalValue = ArrayUtils.findMinimalValue(array1);
         System.out.println(String.format("minimal value = %d", minimalValue));
 
-        System.out.println("Test3: Search Minimal value - Finished\n");
+        System.out.println("Test8: Search Minimal value - Finished\n");
     }
 
     @Test
     public void testArrayUtilsF1() {
-        System.out.println("Test3: Search Maximal value - Started");
+        System.out.println("Test9: Search Maximal value - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -136,12 +136,12 @@ public class ArrayUtilsTest {
         int maximalValue = ArrayUtils.findMaximalValue(array1);
         System.out.println(String.format("maximal value = %d", maximalValue));
 
-        System.out.println("Test3: Search Maximal value - Finished\n");
+        System.out.println("Test9: Search Maximal value - Finished\n");
     }
 
     @Test
     public void testArrayUtilsG1() {
-        System.out.println("Test3: Find All Prime Numbers - Started");
+        System.out.println("Test10: Find All Prime Numbers - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
@@ -150,21 +150,20 @@ public class ArrayUtilsTest {
         int[] primes = ArrayUtils.findAllPrimes(array1);
         System.out.println(String.format("primes: = %s", Arrays.toString(primes)));
 
-        System.out.println("Test3: Find All Prime Numbers - Finished\n");
+        System.out.println("Test10: Find All Prime Numbers - Finished\n");
     }
 
     @Test
     public void testArrayUtilsG2() {
-        System.out.println("Test3: Find All Prime Numbers - Started");
-        int[] arr = new int[]{84, 4, 6, 8, 36, 52};
+        System.out.println("Test11: Find All Fibonacci Numbers - Started");
+        int[] arr = new int[]{7, 3, 4, 6, 8, 5, 1};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
         PlainArray array1 = new Array(arr);
-        System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        int[] primes = ArrayUtils.findAllPrimes(array1);
-        System.out.println(String.format("primes: = %s", Arrays.toString(primes)));
-
-        System.out.println("Test3: Find All Prime Numbers - Finished\n");
+        int[] resultFibonacci = ArrayUtils.findAllFibonacci(array1);
+        System.out.println(String.format("fibonacci values: %s", Arrays.toString(resultFibonacci)));
+        System.out.println("Test10: Find All Fibonacci Numbers - Finished\n");
     }
+
 
 }
