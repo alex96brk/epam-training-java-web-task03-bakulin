e63f70f82a7975615a2628622d55dc247b00c8ec
Alexey <alexey.bakulin96@gmail.com>
2021-10-19 14:57:34 +0300

    [add]: test for base JaggedIntArray functionality added

--- src/by/epamtc/bakulin/task03/test/JaggedIntArrayTest.java
+++ src/by/epamtc/bakulin/task03/test/JaggedIntArrayTest.java
@@ -1,2 +1,23 @@
-package by.epamtc.bakulin.task03.test;public class JaggedIntArrayTest {
+package by.epamtc.bakulin.task03.test;
+
+import by.epamtc.bakulin.task03.entity.JaggedIntegerArray;
+import by.epamtc.bakulin.task03.utils.ConsoleUtils;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JaggedIntArrayTest {
+
+    @Test
+    public void jaggedIntegerArrayClassTest() {
+        ConsoleUtils.printConsoleMessage("Test: Base Class Functionality\n");
+        int matrixDimension = 5;
+        JaggedIntegerArray jaggedIntegerArray = new JaggedIntegerArray(matrixDimension);
+        ConsoleUtils.printConsoleMessage("Populating array with random numerics:\n");
+        jaggedIntegerArray.populateJaggedArrayRandomNumerics(5, 14);
+        ConsoleUtils.printJaggedArray(jaggedIntegerArray);
+
+        ConsoleUtils.printConsoleMessage("Test: Finished\n");
+    }
 }
