c02ef570a0db244aa42d69622db7ba1aa7ef84cf
Alexey <alexey.bakulin96@gmail.com>
2021-10-23 23:34:08 +0300

    [add]: tests for bubble sort added

--- src/by/epamtc/bakulin/task03/test/IntegerArrayUtilsTest.java
+++ src/by/epamtc/bakulin/task03/test/IntegerArrayUtilsTest.java
@@ -1,8 +1,9 @@
 package by.epamtc.bakulin.task03.test;
 
-import by.epamtc.bakulin.task03.entity.DynamicIntegerArray;
+import by.epamtc.bakulin.task03.entity.DynamicArray;
 import by.epamtc.bakulin.task03.entity.Array;
-import by.epamtc.bakulin.task03.utils.IntegerArrayUtils;
+import by.epamtc.bakulin.task03.utils.ArrayUtils;
+import by.epamtc.bakulin.task03.utils.SortType;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -15,12 +16,12 @@ public class IntegerArrayUtilsTest {
     @Test
     public void testArrayUtilsA1() {
         System.out.println("Test1: Bubble sort Ascending - Started");
-        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        Integer[] arr = new Integer[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array<Integer> array1 = new DynamicArray<>(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArrayBubbleAsc(array1);
+        ArrayUtils.sort(array1, SortType.BUBBLE);
         System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
         System.out.println("Test1: Bubble sort - Finished\n");
@@ -28,27 +29,27 @@ public class IntegerArrayUtilsTest {
 
     @Test
     public void testArrayUtilsA2() {
-        System.out.println("Test2: Bubble sort Descending - Started");
-        int[] arr = new int[]{1, 4, 6, 8, 3, 5};
+        System.out.println("Test1: Bubble sort Descending - Started");
+        Integer[] arr = new Integer[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array<Integer> array1 = new DynamicArray<>(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArrayBubbleDesc(array1);
+        ArrayUtils.sort(array1, SortType.BUBBLE, false);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
-        System.out.println("Test2: Bubble sort - Finished\n");
+        System.out.println("Test1: Bubble sort - Finished\n");
     }
-
+/**
     @Test
     public void testArrayUtilsB1() {
         System.out.println("Test3: Selection sort Ascending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArraySelectionAsc(array1);
+        ArrayUtils.sortArraySelectionAsc(array1);
         System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
         System.out.println("Test3: Selection sort - Finished\n");
@@ -59,10 +60,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test4: Selection sort Descending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArraySelectionDesc(array1);
+        ArrayUtils.sortArraySelectionDesc(array1);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
         System.out.println("Test4: Selection sort - Finished\n");
@@ -73,10 +74,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test5: Quick sort Ascending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArrayQuickAsc(array1);
+        ArrayUtils.sortArrayQuickAsc(array1);
         System.out.println(String.format("Sorted Array Ascending: %s", array1.toString()));
 
         System.out.println("Test5: Quick sort - Finished\n");
@@ -87,10 +88,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test6: Quick sort Descending - Started");
         int[] arr = new int[]{1, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        IntegerArrayUtils.sortArrayQuickDesc(array1);
+        ArrayUtils.sortArrayQuickDesc(array1);
         System.out.println(String.format("Sorted Array Descending: %s", array1.toString()));
 
         System.out.println("Test6: Quick sort - Finished\n");
@@ -101,11 +102,11 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test7: Binary Search - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
         int searchValue = 4;
         System.out.println(String.format("search value = %d", searchValue));
-        int resIndex = IntegerArrayUtils.binarySearch(4, array1);
+        int resIndex = ArrayUtils.binarySearch(4, array1);
         System.out.println(String.format("result index = %d", resIndex));
         System.out.println(String.format("Sorted Array: %s", array1.toString()));
         System.out.println("Test7: Binary Search - Finished\n");
@@ -116,10 +117,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test8: Search Minimal value - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        int minimalValue = IntegerArrayUtils.findMinimalValue(array1);
+        int minimalValue = ArrayUtils.findMinimalValue(array1);
         System.out.println(String.format("minimal value = %d", minimalValue));
 
         System.out.println("Test8: Search Minimal value - Finished\n");
@@ -130,10 +131,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test9: Search Maximal value - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        int maximalValue = IntegerArrayUtils.findMaximalValue(array1);
+        int maximalValue = ArrayUtils.findMaximalValue(array1);
         System.out.println(String.format("maximal value = %d", maximalValue));
 
         System.out.println("Test9: Search Maximal value - Finished\n");
@@ -144,10 +145,10 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test10: Find All Prime Numbers - Started");
         int[] arr = new int[]{7, 4, 6, 8, 3, 5};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("Current Array: %s", array1.toString()));
 
-        int[] primes = IntegerArrayUtils.findAllPrimes(array1);
+        int[] primes = ArrayUtils.findAllPrimes(array1);
         System.out.println(String.format("primes: = %s", Arrays.toString(primes)));
 
         System.out.println("Test10: Find All Prime Numbers - Finished\n");
@@ -158,9 +159,9 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test11: Find All Fibonacci Numbers - Started");
         int[] arr = new int[]{7, 3, 4, 6, 8, 5, 1};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
 
-        int[] resultFibonacci = IntegerArrayUtils.findAllFibonacci(array1);
+        int[] resultFibonacci = ArrayUtils.findAllFibonacci(array1);
         System.out.println(String.format("fibonacci values: %s", Arrays.toString(resultFibonacci)));
         System.out.println("Test10: Find All Fibonacci Numbers - Finished\n");
     }
@@ -170,13 +171,13 @@ public class IntegerArrayUtilsTest {
         System.out.println("Test12: Array Reverse - Started");
         int[] arr = new int[]{7, 3, 4, 6, 8, 5, 1, 4, 3};
         System.out.println(String.format("(array): %s", Arrays.toString(arr)));
-        IntegerArrayUtils.arrayReverse(arr);
+        ArrayUtils.arrayReverse(arr);
         System.out.println(String.format("(array) reverse: %s", Arrays.toString(arr)));
 
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
         System.out.println(String.format("(Attempt to add(int[] array): %s", Arrays.toString(arr)));
         System.out.println(String.format("(Added array): %s", array1.toString()));
-        IntegerArrayUtils.arrayReverse(array1);
+        ArrayUtils.arrayReverse(array1);
         System.out.println(String.format("(Reversed array): %s", array1.toString()));
         System.out.println("Test12: Array Reverse - Finished");
     }
@@ -187,9 +188,9 @@ public class IntegerArrayUtilsTest {
 
         int[] arr = new int[]{775, 345, 447, 651, 977, 355, 181};
         System.out.println(String.format("Attempt to add(int[] array): %s", Arrays.toString(arr)));
-        Array array1 = new DynamicIntegerArray(arr);
+        Array array1 = new DynamicArray(arr);
 
-        Array uniqueValues = IntegerArrayUtils.findAllUniqueValues(array1);
+        Array uniqueValues = ArrayUtils.findAllUniqueValues(array1);
         System.out.println(String.format("Unique values: %s", uniqueValues));
 
         System.out.println("Test13: Find All Unique Values - Finished");
@@ -200,8 +201,8 @@ public class IntegerArrayUtilsTest {
     public void testArrayUtilsI1() {
         System.out.println("Test13: Populate existing Array from TXT file - Started");
         int[] arr = new int[]{775, 345, 447, 651, 977, 355, 181};
-        Array array = new DynamicIntegerArray(arr);
-        IntegerArrayUtils.populateIntegerArrayFromTxtFile(array, "E:/EPAM Projects/epam-training-java-web-task03-bakulin/resources/array.txt", 0);
+        Array array = new DynamicArray(arr);
+        ArrayUtils.populateIntegerArrayFromTxtFile(array, "E:/EPAM Projects/epam-training-java-web-task03-bakulin/resources/array.txt", 0);
         System.out.println(array);
         System.out.println("Test13: Populate existing Array from TXT file - Started");
     }
@@ -209,11 +210,11 @@ public class IntegerArrayUtilsTest {
     @Test
     public void testArrayUtilsI2() {
         System.out.println("Test13: Populate new Array from TXT file - Started");
-        Array array = IntegerArrayUtils.populateIntegerArrayFromTxtFile("E:/EPAM Projects/epam-training-java-web-task03-bakulin/resources/array.txt", 1);
+        Array array = ArrayUtils.populateIntegerArrayFromTxtFile("E:/EPAM Projects/epam-training-java-web-task03-bakulin/resources/array.txt", 1);
         System.out.println(array);
         System.out.println("Test13: Populate new Array from TXT file - Started");
     }
 
 
-
+*/
 }
