72961b4b581eb82c5f6291e8fa8ae152ee04ef75
Alexey <alexey.bakulin96@gmail.com>
2021-10-23 20:27:20 +0300

    [delete]: comporator, iterator

--- src/by/epamtc/bakulin/task03/entity/comporator/BubbleSortComparator.java
+++
@@ -1,6 +0,0 @@
-package by.epamtc.bakulin.task03.entity.comporator;
-
-import java.util.Comparator;
-
-public class BubbleSortComporator <E extends Number> implements Comparator<E> {
-}
--- src/by/epamtc/bakulin/task03/entity/iterator/ArrayIterator.java
+++
@@ -1,2 +0,0 @@
-package by.epamtc.bakulin.task03.entity.iterator;public interface ArrayIterator {
-}
