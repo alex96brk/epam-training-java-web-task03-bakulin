2f91aef764c3f3c9efa0df577a7a3214cc6925c9
Alexey <alexey.bakulin96@gmail.com>
2021-10-26 13:09:53 +0300

    [edit]: add generic functionality

--- src/by/epamtc/bakulin/task03/entity/JaggedMatrix.java
+++ src/by/epamtc/bakulin/task03/entity/JaggedMatrix.java
@@ -1,85 +1,92 @@
 package by.epamtc.bakulin.task03.entity;
 
-import by.epamtc.bakulin.task03.utils.PlainRandomGenerator;
+public class JaggedMatrix<E extends Number & Comparable> implements SquareMatrix<E> {
 
-public class JaggedIntegerArray {
+    private static final Object[][] EMPTY_MATRIX = new Object[0][0];
 
-    /**
-     * Двумерный массив
-     */
-    private int[][] arrayData;
+    private Object[][] matrixData;
 
-    /**
-     * Размер квадратной матрицы
-     */
-    private int matrixSize;
-
-    /**
-     * Конструктор для создания пустого зубчатого массива
-     * @param matrixRange
-     */
-    public JaggedIntegerArray(int matrixRange) {
-        this.matrixSize = matrixRange;
-        this.arrayData = generateJaggedArray(matrixRange);
+    public JaggedMatrix(E[][] jaggedArray) {
+        this.matrixData = jaggedArray;
+    }
+    public JaggedMatrix() {
+        this.matrixData = EMPTY_MATRIX;
     }
 
-    /**
-     * Вызов двумерного массива
-     * @return int[][] jagged
-     */
-    public int[][] getArrayData() {
-        return this.arrayData;
+    @Override
+    public void setElement(int row, int col, E value) {
+        checkPosition(row, col);
+        this.matrixData[row][col] = value;
     }
 
-    /**
-     * Присвоить данные зубчатому массиву
-     * @return int[][] jagged
-     */
-    public void setArrayData(int[][] arrayData) {
-        this.arrayData = arrayData;
+    @Override
+    public E getElement(int row, int col) {
+        checkPosition(row, col);
+        return (E) this.matrixData[row][col];
     }
 
-    /**
-     * Возвращает размер квадратной матрицы
-     * @return
-     */
-    public int getJaggedArraySize() {
-        return matrixSize;
+    @Override
+    public E[] getRow(int row) {
+        checkPosition(row);
+        return (E[]) this.matrixData[row];
     }
 
-    /**
-     * Заполняет зубчатый массив случайными числами
-     * @param randomStartValue начальный аргумент для генерации случайных числе
-     * @param increment инкремент функции генерации случайных чисел
-     */
-    public void populateJaggedArrayRandomNumerics(int randomStartValue, int increment) {
-        PlainRandomGenerator randomGenerator = new PlainRandomGenerator(31, increment, 127, randomStartValue);
+    @Override
+    public E[][] getMatrixData() {
+        return (E[][])this.matrixData;
+    }
 
-        for (int i = 0; i < arrayData.length; i++) {
+    @Override
+    public void setMatrixData(E[][] matrixData) {
+        this.matrixData = matrixData;
+    }
 
-            for (int j = 0; j < arrayData[i].length; j++) {
-                arrayData[i][(j)] = randomGenerator.calculateRandom();
-            }
+    @Override
+    public int getMatrixSize() {
+        return 0;
+    }
+
+    private boolean checkPosition(int row, int col) {
+        boolean result = false;
+        if (row < 0 || row > matrixData.length) {
+            throw new IndexOutOfBoundsException(String.format("Illegal row index: row = %d", row));
         }
-    }
-
-    /**
-     * Генерирует зубчатый массив
-     * @param matrixRange размер квадратной матрицы
-     * @return зубчатый массив int[][]
-     */
-    private static int[][] generateJaggedArray(int matrixRange) {
-        int[][] result = new int[matrixRange][];
-
-        for (int i = 0; i < matrixRange; i++) {
-            result[i] = new int[matrixRange - i];
-
-            if (i == (matrixRange - 1)) {
-                result[i] = new int[1];
-            }
+        if (col < 0 || col > matrixData[row].length) {
+            throw new IndexOutOfBoundsException(String.format("Illegal column index for row = %d: column = %d", row, col));
+        }
+        if ((row > 0 && row < matrixData.length) && (col > 0 && col < matrixData[row].length)) {
+            result = true;
         }
         return result;
     }
+    private boolean checkPosition(int row) {
+        boolean result = false;
+        if (row < 0 || row > matrixData.length) {
+            throw new IndexOutOfBoundsException(String.format("Illegal row index: row = %d", row));
+        }
+        if ((row > 0 && row < matrixData.length)) {
+            result = true;
+        }
+        return result;
+    }
+
+
+//    /**
+//     * Заполняет зубчатый массив случайными числами
+//     * @param randomStartValue начальный аргумент для генерации случайных числе
+//     * @param increment инкремент функции генерации случайных чисел
+//     */
+//    public void populateJaggedArrayRandomNumerics(int randomStartValue, int increment) {
+//        PlainRandomGenerator randomGenerator = new PlainRandomGenerator(31, increment, 127, randomStartValue);
+//
+//        for (int i = 0; i < arrayData.length; i++) {
+//
+//            for (int j = 0; j < arrayData[i].length; j++) {
+//                arrayData[i][(j)] = randomGenerator.calculateRandom();
+//            }
+//        }
+//    }
+
 
 
 }
