a92d4b2dfaebf826ba53905a2e7cd8fc19c28f6c
Alexey <alexey.bakulin96@gmail.com>
2021-10-26 14:35:37 +0300

    [add]: sort row elements generic impl added

--- src/by/epamtc/bakulin/task03/test/JaggedMatrixTest.java
+++ src/by/epamtc/bakulin/task03/test/JaggedMatrixTest.java
@@ -28,39 +28,35 @@ public class JaggedMatrixTest {
 
 
     @Test
-    public void jaggedIntegerArrayUtilsClassTest() {
+    public void jaggedIntegerArrayUtilsClassTest1() {
         SquareMatrix<Integer> jaggedMatrix = new JaggedMatrix(JaggedMatrixUtils.generateJaggedArrayInteger(5));
         JaggedMatrixUtils.populateJaggedMatrixRandomInteger(jaggedMatrix, 5, 7);
         ConsoleUtils.printJaggedArray(jaggedMatrix);
         JaggedMatrixUtils.sortJaggedArrayBySumRowsElements(jaggedMatrix);
         ConsoleUtils.printConsoleMessage("After sort:\n");
         ConsoleUtils.printJaggedArray(jaggedMatrix);
-//        ConsoleUtils.printConsoleMessage("Test: Base Class Functionality\n");
-//        int matrixDimension = 5;
-//        JaggedMatrix jaggedMatrix = new JaggedMatrix(matrixDimension);
-//        ConsoleUtils.printConsoleMessage("Populating array with random numerics:\n");
-//
-//        jaggedMatrix.populateJaggedArrayRandomNumerics(5, 7);
-//        ConsoleUtils.printJaggedArray(jaggedMatrix);
-//
-//        JaggedIntegerArrayUtils.sortJaggedArrayByMaxSumRowsElementsAsc(jaggedMatrix);
-//        ConsoleUtils.printConsoleMessage("\nSorted jagged array ascending row sums:\n");
-//        ConsoleUtils.printJaggedArray(jaggedMatrix);
-//
-//        ConsoleUtils.printConsoleMessage("Test: Finished\n");
     }
+
     @Test
-    public void test() {
-//        Integer[] nums = new Integer[] {23, 15, 32, 41};
-//        Integer sum = JaggedMatrixUtils.calculateRowSum(nums);
-//        System.out.println(sum);
+    public void jaggedIntegerArrayUtilsClassTest2() {
         SquareMatrix<Integer> jaggedMatrix = new JaggedMatrix(JaggedMatrixUtils.generateJaggedArrayInteger(5));
         JaggedMatrixUtils.populateJaggedMatrixRandomInteger(jaggedMatrix, 5, 7);
         ConsoleUtils.printJaggedArray(jaggedMatrix);
-        JaggedMatrixUtils.sortJaggedArrayBySumRowsElements(jaggedMatrix);
-        ConsoleUtils.printConsoleMessage("Sorted");
+        JaggedMatrixUtils.sortJaggedArrayRowElements(jaggedMatrix, false);
+        ConsoleUtils.printConsoleMessage("Sorted:\n");
         ConsoleUtils.printJaggedArray(jaggedMatrix);
     }
+
+    @Test
+    public void jaggedIntegerArrayUtilsClassTest3() {
+        SquareMatrix<Integer> jaggedMatrix = new JaggedMatrix(JaggedMatrixUtils.generateJaggedArrayInteger(5));
+        JaggedMatrixUtils.populateJaggedMatrixRandomInteger(jaggedMatrix, 5, 7);
+        ConsoleUtils.printJaggedArray(jaggedMatrix);
+        ConsoleUtils.printConsoleMessage("Sorted:\n");
+        JaggedMatrixUtils.sortJaggedArrayRowElements(jaggedMatrix, true);
+        ConsoleUtils.printJaggedArray(jaggedMatrix);
+    }
+
 //
 //    @Test
 //    public void jaggedIntegerArrayUtilsClassTest1() {
