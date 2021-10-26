b08f2868e178cde20ce49fb385dfcf7f0adaba7a
Alexey <alexey.bakulin96@gmail.com>
2021-10-26 13:10:24 +0300

    [add]: abstraction added to Jagged Arrays

--- src/by/epamtc/bakulin/task03/entity/SquareMatrix.java
+++ src/by/epamtc/bakulin/task03/entity/SquareMatrix.java
@@ -1,2 +1,15 @@
-package by.epamtc.bakulin.task03.entity;public interface SquareMatrix {
+package by.epamtc.bakulin.task03.entity;
+
+public interface SquareMatrix <E> {
+    void setElement(int row, int col, E value);
+
+    E getElement(int row, int col);
+
+    E[] getRow(int row);
+
+    E[][] getMatrixData();
+
+    void setMatrixData(E[][] matrixData);
+
+    int getMatrixSize();
 }
