c4b0ee63770c11e7b50c8eedea59d5cbc3ca1fb4
Alexey <alexey.bakulin96@gmail.com>
2021-10-26 13:11:08 +0300

    [edit]: try to improve functionality by generic

--- src/by/epamtc/bakulin/task03/utils/JaggedMatrixUtils.java
+++ src/by/epamtc/bakulin/task03/utils/JaggedMatrixUtils.java
@@ -1,37 +1,124 @@
 package by.epamtc.bakulin.task03.utils;
 
 import by.epamtc.bakulin.task03.entity.JaggedMatrix;
+import by.epamtc.bakulin.task03.entity.SquareMatrix;
 
-public class JaggedIntegerArrayUtils {
+public class JaggedMatrixUtils<E extends Number & Comparable> {
 
-//    /**
-//     * Сортирует зубчатый массив
-//     * в порядке возрастания сумм элементов строки.
-//     *
-//     * @param jaggedMatrix целевой зубчатый массив
-//     */
-//    public static void sortJaggedArrayByMaxSumRowsElementsAsc(JaggedMatrix jaggedMatrix) {
-//        int[] sums = populateSumArray(jaggedMatrix);
-//        int[][] jagged = jaggedMatrix.getArrayData();
-//
-//        for (int i = 0; i < (jagged.length - 1); i++) {
-//            for (int j = (jagged.length - 1); j > i; j--) {
-//                if (sums[j] < sums[(j - 1)]) {
-//                    int buffer = sums[j];
-//                    int[] bufferJag = jagged[j];
-//
-//                    sums[j] = sums[(j - 1)];
-//                    jagged[j] = jagged[(j - 1)];
-//
-//                    sums[(j - 1)] = buffer;
-//                    jagged[(j - 1)] = bufferJag;
-//
-//                }
-//            }
-//        }
-//        jaggedMatrix.setArrayData(jagged);
-//    }
-//
+    /**
+     * Генерирует пустой зубчатый массив типа Integer
+     *
+     * @param matrixRange размер квадратной матрицы
+     * @return зубчатый массив Object[][]
+     */
+    public static <E extends Number> Integer[][] generateJaggedArrayInteger(int matrixRange) {
+        Integer[][] result = new Integer[matrixRange][];
+
+        for (int i = 0; i < matrixRange; i++) {
+            result[i] = new Integer[matrixRange - i];
+
+            if (i == (matrixRange - 1)) {
+                result[i] = new Integer[1];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Генерирует пустой зубчатый массив типа Double
+     *
+     * @param matrixRange размер квадратной матрицы
+     * @return зубчатый массив Object[][]
+     */
+    public static <E extends Number> Double[][] generateJaggedArrayDouble(int matrixRange) {
+        Double[][] result = new Double[matrixRange][];
+
+        for (int i = 0; i < matrixRange; i++) {
+            result[i] = new Double[matrixRange - i];
+
+            if (i == (matrixRange - 1)) {
+                result[i] = new Double[1];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Заполняет зубчатый массив случайными числами
+     *
+     * @param randomStartValue начальный аргумент для генерации случайных числе
+     * @param increment        инкремент функции генерации случайных чисел
+     */
+    public static void populateJaggedMatrixRandomInteger(SquareMatrix<Integer> matrix, int randomStartValue, int increment) {
+        Integer primeA = 31;
+        Integer module = 127;
+        IntegerRandomGenerator randomGenerator = new IntegerRandomGenerator(primeA, increment, module, randomStartValue);
+        Integer[][] matrixData = (Integer[][]) matrix.getMatrixData();
+        for (int i = 0; i < matrixData.length; i++) {
+
+            for (int j = 0; j < matrixData[i].length; j++) {
+                matrixData[i][j] = randomGenerator.calculateRandom();
+            }
+        }
+    }
+
+    /**
+     * Сортирует зубчатый массив
+     * в порядке возрастания сумм элементов строки.
+     *
+     * @param jaggedIntegerArray целевой зубчатый массив
+     */
+    public static void sortJaggedArrayBySumRowsElementsAsc(SquareMatrix<Integer> jaggedIntegerArray) {
+        Integer[] sums = populateSumArray(jaggedIntegerArray);
+        Integer[][] jagged = jaggedIntegerArray.getMatrixData();
+
+        for (int i = 0; i < (jagged.length - 1); i++) {
+            for (int j = (jagged.length - 1); j > i; j--) {
+                if (sums[j] < sums[(j - 1)]) {
+                    int buffer = sums[j];
+                    Integer[] bufferJag = jagged[j];
+
+                    sums[j] = sums[(j - 1)];
+                    jagged[j] = jagged[(j - 1)];
+
+                    sums[(j - 1)] = buffer;
+                    jagged[(j - 1)] = bufferJag;
+
+                }
+            }
+        }
+        jaggedIntegerArray.setMatrixData(jagged);
+    }
+
+    private static Integer[] populateSumArray(SquareMatrix<Integer> jaggedMatrix) {
+        Integer[][] jagged = (Integer[][]) jaggedMatrix.getMatrixData();
+        Integer[] sums = new Integer[jagged.length];
+
+        for (int i = 0; i < jagged.length; i++) {
+            for (int j = 0; j < jagged[i].length; j++) {
+                sums[i] = calculateRowSum(jagged[i]);
+            }
+        }
+        return sums;
+    }
+
+    /**
+     * Вычисляет сумму элементов массива (строки в зубчатом массиве)
+     *
+     * @param row массив(строка в зубчатом массиве)
+     * @return int sum - сумма элементов массива(строки)
+     */
+    private static Integer calculateRowSum(Integer[] row) {
+        Integer sum = 0;
+        for (int i = 0; i < row.length; i++) {
+            sum = sum + row[i];
+        }
+        return sum;
+    }
+
+
+
+
 //    /**
 //     * Сортирует зубчатый массив
 //     *
